596d3981628fe12f7bd88c63cec633fa7ba1aed4
Anders Carling <anders.carling@d05.se>
2011-12-08 17:21:28 +0100

    Check OpenID auth connection using CA certificates on Linux

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,12 @@
 require 'openid/store/filesystem'
 
+# Should handle CA certificates on Linux (at least Debian-ish Linux)
+ca_path = "/etc/ssl/certs/ca-certificates.crt"
+if File.readable?(ca_path)
+  require "openid/fetchers"
+  OpenID.fetcher.ca_file = ca_path
+end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :openid,
     name:       'google',
